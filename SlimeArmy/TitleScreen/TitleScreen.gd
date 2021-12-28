673550e43ef69d50ce411bdb92c612e2605805b0
Anna-V <57063500+Anna-Van@users.noreply.github.com>
2021-12-28 20:08:28 +0100

    Sounds at TitleScreen

--- SlimeArmy/TitleScreen/TitleScreen.gd
+++ SlimeArmy/TitleScreen/TitleScreen.gd
@@ -1,13 +1,17 @@
 extends Node
 
 func _ready():
+	$Sounds/Music.play(0)
 	pass
 
 func _on_NewGame_pressed():
+	$Sounds/Button.play(0)
 	get_tree().change_scene("res://MainScene/Main.tscn");
 
 func _on_Options_pressed():
+	$Sounds/Button.play(0)
 	get_tree().change_scene("res://Options/Options.tscn");
 
 func _on_Quit_pressed():
+	$Sounds/Button.play(0)
 	get_tree().quit();
